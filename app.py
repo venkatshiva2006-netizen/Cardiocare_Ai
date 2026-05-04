ea44cc43ce8eb73bcb8c4ba69e32261afd805c86
Venkat S <venkatshiva2006@gmail.com>
2026-05-04 21:55:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.secret_key = 'super_secret_university_key_change_later'
 # ==========================================
 # IMPORTANT: PASTE YOUR GEMINI API KEY HERE
 # ==========================================
-gemini_client = genai.Client(api_key="my api key")
+gemini_client = genai.Client(api_key="my api key ")
 
 print("Waking up the AI Brains...")
 try:
